c970f55bef2cc45f79796b99f461be409d5bf776
Silver CHEN <silver@sharelug.com>
2021-05-19 08:03:06 +0800

    init2

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 1. bring up master and slave
+# 2. get master binlog name and position
+# 3. setup slave
+# 4. done
+
+repl_user="CREATE USER 'repl'@'%' IDENTIFIED BY 'dev1234';GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';"
+docker exec mysql_master sh -c "mysql -u root -pdev1234 -e '$repl_user'"
