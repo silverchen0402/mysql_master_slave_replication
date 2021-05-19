64427a515bc0cad83de932d38026096c5a54b5e2
Silver CHEN <silver@sharelug.com>
2021-05-19 11:59:15 +0800

    Wed May 19 11:59:15 CST 2021

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ echo $name "=" $value1
 line=`grep -P "Slave_SQL_Running: " $fn0`
 IFS=$' ' read -r name value2 <<<"$line"
 echo $name "=" $value2
-rm $fn0
+#rm $fn0
 if [[ "$value1" == "Yes" || "$value2" == "Yes" ]];
 then
   echo "Replication set, try some sql"
